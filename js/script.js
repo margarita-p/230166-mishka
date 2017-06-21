1fa126d697ac0652021246b697ea47b9a730e334
margarita-p <dayzzzy@gmail.com>
2017-06-21 03:44:18 +0300

    Правки

--- js/script.js
+++ js/script.js
@@ -2,9 +2,12 @@ var modalOpen = document.querySelector(".js-modal-open");
 var modalSize = document.querySelector(".modal-size");
 var modalClose = document.querySelector(".js-modal-close");
 
-// var menuOpen = document.querySelector(".page-header__hamburger");
-// var menu = document.querySelector(".main-nav");
-// var menuNoJs = document.querySelector(".main-nav__no-js");
+var menuToggle = document.querySelector(".hamburger");
+var menuOpen = document.querySelector(".hamburger__open");
+var menu = document.querySelector(".main-nav");
+var menuClose = document.querySelector(".hamburger__close");
+
+
 
 modalOpen.addEventListener("click", function(evt) {
   evt.preventDefault();
@@ -25,19 +28,24 @@ window.addEventListener("keydown", function(evt) {
 });
 
 
-// menu.addEventListener("DOMContentLoaded", function(evt) {
-//   if (menu.classList.contains("main-nav__no-js")) {
-//     (menu.classList.remove("main-nav__no-js"));
-//   }
-// })
-//
-//
-// menuOpen.addEventListener("click", function(evt) {
-//   evt.preventDefault();
-//   if (!menu.classList.contains("main-nav__show")) {
-//     menu.classList.add("main-nav__show");
-//   }
-//   else {
-//     menu.classList.remove("main-nav__show");
-//   }
-// });
+
+document.addEventListener("DOMContentLoaded", function(evt) {
+  evt.preventDefault();
+  if(menu.classList.contains("main-nav--no-js")) {
+    menu.classList.remove("main-nav--no-js");
+  }
+});
+
+menuToggle.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  if (!menu.classList.contains("main-nav__show")) {
+    menu.classList.add("main-nav__show");
+    menuClose.classList.add("hamburger__show");
+    menuOpen.classList.add("hamburger__hide");
+  }
+  else {
+    menu.classList.remove("main-nav__show");
+    menuClose.classList.remove("hamburger__show");
+    menuOpen.classList.remove("hamburger__hide");
+  }
+});
