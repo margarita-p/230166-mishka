529852debf8f072de3e782cdf14aa26f64a22073
margarita-p <dayzzzy@gmail.com>
2017-06-25 03:10:19 +0300

    Итоговая проверка 2

--- js/script.js
+++ js/script.js
@@ -3,26 +3,24 @@ var modalSize = document.querySelector(".modal-size");
 var modalClose = document.querySelector(".js-modal-close");
 
 var menuToggle = document.querySelector(".hamburger");
-var menuOpen = document.querySelector(".hamburger__open");
 var menu = document.querySelector(".main-nav");
-var menuClose = document.querySelector(".hamburger__close");
 
 
 
 modalOpen.addEventListener("click", function(evt) {
   evt.preventDefault();
-  modalSize.classList.add("modal-size__show");
+  modalSize.classList.add("modal-size--show");
 });
 
 modalClose.addEventListener("click", function(evt) {
   evt.preventDefault();
-  modalSize.classList.remove("modal-size__show");
+  modalSize.classList.remove("modal-size--show");
 });
 
 window.addEventListener("keydown", function(evt) {
   if (evt.keyCode === 27) {
-    if (modalSize.classList.contains("modal-size__show")) {
-      modalSize.classList.remove("modal-size__show");
+    if (modalSize.classList.contains("modal-size--show")) {
+      modalSize.classList.remove("modal-size--how");
     }
   }
 });
@@ -34,18 +32,27 @@ document.addEventListener("DOMContentLoaded", function(evt) {
   if(menu.classList.contains("main-nav--no-js")) {
     menu.classList.remove("main-nav--no-js");
   }
+  if (menuToggle.classList.contains("hamburger--no-js")) {
+    menuToggle.classList.remove("hamburger--no-js");
+  }
 });
 
 menuToggle.addEventListener("click", function(evt) {
   evt.preventDefault();
-  if (!menu.classList.contains("main-nav__show")) {
-    menu.classList.add("main-nav__show");
-    menuClose.classList.add("hamburger__show");
-    menuOpen.classList.add("hamburger__hide");
+  if (!menu.classList.contains("main-nav--show")) {
+    menu.classList.add("main-nav--show");
+    menuToggle.classList.add("hamburger--close");
   }
   else {
-    menu.classList.remove("main-nav__show");
-    menuClose.classList.remove("hamburger__show");
-    menuOpen.classList.remove("hamburger__hide");
+    menu.classList.remove("main-nav--show");
+    menuToggle.classList.remove("hamburger--close");
+  }
+});
+
+window.addEventListener("keydown", function(evt) {
+  if (evt.keyCode === 27) {
+    if (menu.classList.contains("main-nav--show")) {
+      menu.classList.remove("main-nav--show");
+    }
   }
 });
